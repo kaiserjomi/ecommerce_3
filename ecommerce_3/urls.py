342bbad8017f2ad8999c8eb20d8925122a06b0b1
kaiserpt <jomi.man@gmail.com>
2022-12-16 16:09:12 +0000

    django-admin startproject ecommerce_3 .

--- ecommerce_3/urls.py
+++ ecommerce_3/urls.py
@@ -1,4 +1,4 @@
-"""ecommerce3 URL Configuration
+"""ecommerce_3 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- ecommerce_3/wsgi.py
+++ ecommerce_3/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ecommerce3 project.
+WSGI config for ecommerce_3 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_3.settings')
 
 application = get_wsgi_application()
